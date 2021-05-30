9d1665166c6c67512924d01324b23a5b1b916ec8
John Doe <myemail>
2021-05-31 03:07:35 +0500

    file

---
+++ file.py
@@ -0,0 +1,19 @@
+from tkinter import *
+#from gui import format_time
+window=Tk()
+window.geometry('350x200')
+window.title('File Handling')
+file_entry=Entry(window,width=10)
+lbl=Label(window,text='File name:')
+lbl.pack()
+file_entry.pack()
+def read():
+    try:
+        infile=open(file_entry.get())
+        lbl=Label(window,text=infile.read())
+        lbl.pack()
+    except FileNotFoundError as err:
+        print(err)
+btn=Button(window,text='read file',command=read)
+btn.pack()
+window.mainloop()
